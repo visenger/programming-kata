b8c68d9d50dfc06b54d8fa2b285c142ef0418511
visenger <visenger@gmail.com>
2017-01-26 16:34:01 +0100

    exception handling classes;

---
+++ src/main/java/de/bhtb/exception/handling/CoffeeMakerPlayground.java
@@ -0,0 +1,11 @@
+package de.bhtb.exception.handling;
+
+/**
+ * Created by visenger on 11/01/17.
+ */
+public class CoffeeMakerPlayground {
+
+    public static void main(String... args) throws NoCoffeeException {
+        CoffeeMaker coffeeMaker = new CoffeeMaker(2);
+    }
+}
